91281013137cde08c7e040120947a35f37b489ec
TaiNT <tai.nguyentrung1996@gmail.com>
2019-03-22 01:11:35 +0700

    Done Task with simple_form haml and function controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :task
+  resources :tasks do
+    get 'task/index'
+  end
   root "tasks#index"
 end
